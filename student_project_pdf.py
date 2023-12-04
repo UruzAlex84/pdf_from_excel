693a711b3c211ad08e3569d0115fb46747650c04
Alexander <uruzalex@gmail.com>
2023-12-04 22:23:52 +0300

    Finish student project - add text to PDF file

--- student_project_pdf.py
+++ student_project_pdf.py
@@ -9,8 +9,17 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 pdf_name = "student_project"
 for filepath in filepaths:
     pdf.add_page()
+
+    # Add header
     filename = Path(filepath).stem
     name = filename.title()
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"{name}")
+    pdf.cell(w=50, h=8, txt=f"{name}", ln=1)
+
+    # Add text
+    with open(filepath, 'r') as file:
+        content = file.read()
+    pdf.set_font(family="Times", size=12)
+    pdf.multi_cell(w=0, h=8, txt=content)
+
 pdf.output(fr"PDFs\{pdf_name}.pdf")
